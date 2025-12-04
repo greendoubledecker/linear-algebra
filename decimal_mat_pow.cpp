e80711edad6b81cf7e983f6f243629b7391f628e
greendoubledecker <95518384+greendoubledecker@users.noreply.github.com>
2025-12-04 23:30:22 +0800

    Decimals now.
    This program exponentiates matrices with decimals inside.

---
+++ decimal_mat_pow.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <vector>
+#include <iomanip>
+
+using namespace std;
+
+vector<vector<long double>> mulMatrix(vector<vector<long double>> matrix1, vector<vector<long double>> matrix2, int size){
+	vector<vector<long double>> output(size, vector<long double>(size));
+	for(int row = 0; row < size; ++row){
+		for(int col = 0; col < size; ++col){
+			vector<long double> tgtRow = matrix1[row];
+			long double sum = 0;
+			for(int i = 0; i < size; ++i){
+				long double inCol = matrix2[i][col];
+				long double inRow = tgtRow[i];
+				sum += inCol * inRow;
+			}
+			output[row][col] = sum;
+		}
+	}
+	return output;
+}
+
+int main(){
+	int size;
+	cout << setprecision(18);
+	cout << "Input the size of your matrix." << endl;
+	cin >> size;
+	cout << "Now input the matrix itself." << endl;
+	vector<vector<long double>> matrix(size, vector<long double>(size));
+	for(int i = 0; i < size; ++i){
+		for(int j = 0; j < size; ++j){
+			cin >> matrix[i][j];
+		}
+	}
+	int pow = 0;
+	cout << "Input the power you want the matrix raised to." << endl;
+	cin >> pow;
+	if(pow == 0){
+		cout << "The 0th power is the identity matrix, or" << endl;
+		for(int i = 0; i < size; ++i){
+			for(int j = 0; j < size; ++j){
+				if(i == j){
+					cout << 1;
+				}else{
+					cout << 0;
+				}
+			}
+			cout << endl;
+		}
+		return 0;
+	}
+	if(pow < 0){
+		cout << "MathError: Please do not input a negative power." << endl;
+		return 0;
+	}
+	vector<vector<long double>> result = matrix;
+	for(int i = 1; i < pow; ++i){
+		result = mulMatrix(matrix, result, size);
+	}
+	cout << "The " << pow << "th power of the matrix is" << endl;
+	for(int i = 0; i < size; ++i){
+		for(int j = 0; j < size; ++j){
+			cout << result[i][j] << " ";
+		}
+		cout << endl;
+	}
+	return 0;
+}
